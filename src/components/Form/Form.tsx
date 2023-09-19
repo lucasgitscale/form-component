19166a769f3ba3f471eaf86a1404031f3ab9900e
Lucas Ferreira Scale <lucas.ferreira@contractors.scale.com>
2023-09-19 15:23:50 -0300

    form hook validation done

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,55 @@
 import React from "react";
 import { useLucasForm } from "../../hooks/useLucasForm";
-
+const CITIES = ["Paris", "London", "New York"];
 function Form() {
   const lucasForm = useLucasForm({
     initialValues: {
       name: {
         value: "",
-        required: true,
-        minLength: 3,
-        maxLength: 10,
-        type: "string",
+      },
+      age: {
+        value: "",
+      },
+      email: {
+        value: "",
+      },
+      city: {
+        value: CITIES[0],
+      },
+    },
+    // we can define the validation rules for each field
+    schemaValidation: {
+      name: (inputValue: any) => {
+        if (inputValue === "") {
+          return "Name is required";
+        } else if (inputValue.length < 3) {
+          return "Name must be at least 3 characters long";
+        } else if (inputValue.length > 20) {
+          return "Name must be at most 20 characters long";
+        }
+      },
+      email: (inputValue: any) => {
+        if (inputValue === "") {
+          return "Email is required";
+        } else if (!inputValue.includes("@")) {
+          return "Email must contain @";
+        }
+      },
+      age: (inputValue: any) => {
+        if (!inputValue) {
+          return "Age is required";
+        } else if (inputValue < 18) {
+          console.log("inputValue  mayor a 18===> ", inputValue);
+          return "You must be at least 18 years old";
+        } else if (inputValue > 99) {
+          return "You must be at most 99 years old";
+        }
+      },
+      city: (inputValue: any) => {
+        console.log("inputValue ===> ", inputValue, inputValue === "");
+        if (inputValue === "") {
+          return "City is required";
+        }
       },
     },
     onSubmit: (values) => {
@@ -19,7 +59,7 @@ function Form() {
 
   return (
     <div className="flex h-screen border-b-2 bg-slate-300 ">
-      <div className="container border-blue-0 bg-indigo-100 mx-auto grid place-items-center">
+      <div className="container border-blue-0 bg-indigo-100 mx-auto grid place-items-center ">
         <form
           onSubmit={lucasForm.handleSubmit}
           className="border-gray-100 bg-slate-300 border-3 w-1/3  h-1/3 grid place-items-center rounded-md"
@@ -30,13 +70,52 @@ function Form() {
               type="text"
               name="name"
               onChange={lucasForm.handleChange}
-              value={lucasForm.values.name}
+              value={lucasForm.values.name.value}
+              onBlur={lucasForm.handleBlur}
             />
+            {lucasForm.errors.name && lucasForm.errors.name}
+          </div>
+          <div>
+            <label htmlFor="age"></label>
+            <input
+              type="number"
+              name="age"
+              onChange={lucasForm.handleChange}
+              value={lucasForm.values.age.value}
+              onBlur={lucasForm.handleBlur}
+            />
+            <div className="flex">
+              {lucasForm.errors.age && lucasForm.errors.age}
+            </div>
+          </div>
+          <div>
+            <label htmlFor="city" />
+            <select
+              onChange={lucasForm.handleChange}
+              name="city"
+              value={lucasForm.values.city.value}
+              onBlur={lucasForm.handleBlur}
+            >
+              {CITIES.map((city) => (
+                <option key={city} value={city}>
+                  {city}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="flex gap-3">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+                lucasForm.onSubmitting ||
+                Object.values(lucasForm.errors).length > 0
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
               type="submit"
+              disabled={
+                lucasForm.onSubmitting ||
+                Object.values(lucasForm.errors).length > 0
+              }
             >
               Submit
             </button>
@@ -47,6 +126,9 @@ function Form() {
             >
               Reset
             </button>
+          </div>
+          <div className="flex">
+            {lucasForm.generalError ? lucasForm.generalError : null}
           </div>
         </form>
       </div>
--- src/hooks/useLucasForm.ts
+++ src/hooks/useLucasForm.ts
@@ -1,18 +1,18 @@
-
 import React, { useCallback } from 'react'
+
+
 type inputTypes = string | number | boolean | (string | number[])
 interface IForm {
   onSubmit: (values: object) => void
-  schemaValidation?: (values: object) => object
+  schemaValidation?: Record<string, (value: any) => any>
 
   initialValues: Record<string, {
-    type: inputTypes
+    type?: inputTypes
     value: any,
-    required?: boolean,
-    minLength?: number,
-    maxLength?: number,
+    touched?: boolean,
   }>
 }
+
 
 function useLucasForm({
   onSubmit,
@@ -20,31 +20,93 @@ function useLucasForm({
   initialValues
 }: IForm) {
   const [values, setValues] = React.useState<Record<string, any>>(initialValues)
-  // const [errors, setErrors] = React.useState<Record<string, any>>({})
-  // const [touched, setTouched] = React.useState<Record<string, any>>({})
+  const [generalError, setGeneralError] = React.useState<any>()
+  const [errors, setErrors] = React.useState<Record<string, any>>({})
   const [onSubmitting, setOnSubmitting] = React.useState<boolean>(false)
 
+  const validate = useCallback((values: any) => {
+    const errors: any = {}
 
-  const handleSubmit = useCallback(async (e: { preventDefault: any }) => {
-    debugger
-    e.preventDefault()
-    setOnSubmitting(true)
-    await onSubmit(values)
-  }, [onSubmit, values])
-  // onchange event 
+    if (!schemaValidation) {
+      return errors
+    }
+    const data = Object.entries(schemaValidation)
+
+    if (!data.every(([key]) => key in values)) {
+      throw new Error('The key of the schema must be the same as the key of the values')
+    }
+    data.forEach(([key, fn]) => {
+      const error = fn(values[key].value)
+      if (error) {
+        errors[key] = error
+      }
+    })
+
+    return errors
+  }, [schemaValidation])
 
   const handleChange = (event: { preventDefault: () => void; target: { name: any; value: any } }) => {
-    event.preventDefault()
     setValues((values) => ({
       ...values,
-      [event.target.name]: event.target.value
+      [event.target.name]: {
+        ...values[event.target.name],
+        value: event.target.value,
+      }
     }))
   }
-  const handleReset = () => {
+
+  const handleBlur = useCallback((event: any) => {
+    if (!!event.target) {
+      setValues((values) => ({
+        ...values,
+        [event.target.name]: {
+          ...values[event.target.name],
+          touched: true,
+        }
+      }))
+      setErrors(validate(values))
+    }
+  }, [validate, values])
+  console.log(values)
+
+
+  const handleReset = useCallback(() => {
     setValues(initialValues)
-  }
+    setGeneralError(null)
+    setOnSubmitting(false)
+    setErrors({})
+  }, [initialValues])
+
+  const handleSubmit = useCallback(async (e: { preventDefault: any }) => {
+    e.preventDefault()
+    if (Object.keys(validate(values)).length > 0) {
+      setErrors(validate(values))
+      return
+    }
+    setOnSubmitting(true)
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 4000))
+      await onSubmit(values)
+      setOnSubmitting(false)
+      handleReset()
+    } catch (error: any) {
+      setGeneralError(error.message)
+      setOnSubmitting(false)
+      console.error(error)
+    }
+  }, [handleReset, onSubmit, validate, values])
+
   return (
-    { values, handleChange, handleSubmit, handleReset, onSubmitting }
+    {
+      values,
+      errors,
+      handleChange,
+      handleSubmit,
+      handleReset,
+      handleBlur,
+      onSubmitting,
+      generalError
+    }
   )
 }
 export { useLucasForm }
